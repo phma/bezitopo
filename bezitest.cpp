2673bb893541c19673b133980429d2bd2a44e5a1
Pierre Abbat <phma@phma.optus.nu>
2012-08-10 00:37:12 -0400

    testintegertrig doesn't output huge ream about to check BSD/Linux difference

--- bezitest.cpp
+++ bezitest.cpp
@@ -22,20 +22,39 @@
 
 using namespace std;
 
+void testintegertrig()
+{
+  double sinerror,coserror,ciserror,totsinerror,totcoserror,totciserror;
+  int i;
+  for (totsinerror=totcoserror=totciserror=i=0;i<128;i++)
+  {
+    sinerror=sin(i<<24)+sin((i+128)<<24);
+    coserror=cos(i<<24)+cos((i+128)<<24);
+    ciserror=hypot(cos(i<<24),sin((i+128)<<24))-1;
+    if (sinerror>0.04 || coserror>0.04 || ciserror>0.04)
+    {
+      printf("sin(%8x)=%a sin(%8x)=%a\n",i<<24,sin(i<<24),(i+128)<<24,sin((i+128)<<24));
+      printf("cos(%8x)=%a cos(%8x)=%a\n",i<<24,cos(i<<24),(i+128)<<24,cos((i+128)<<24));
+      printf("abs(cis(%8x))=%a\n",i<<24,hypot(cos(i<<24),sin((i+128)<<24)));
+    }
+    totsinerror+=sinerror*sinerror;
+    totcoserror+=coserror*coserror;
+    totciserror+=ciserror*ciserror;
+  }
+  printf("total sine error=%e\n",totsinerror);
+  printf("total cosine error=%e\n",totcoserror);
+  printf("total cis error=%e\n",totciserror);
+}
+
 int main(int argc, char *argv[])
 {int i,j,itype;
  randfil=fopen("/dev/urandom","rb");
  xy a(0,0),b(4,0),c(0,3),d(4,4),e;
- //printf("Area is %7.3f\n",area3(c,a,b));
  assert(area3(c,a,b)==6);
  lozenge(100);
  rotate(30);
  printf("sin(int)=%f sin(float)=%f\n",sin(65536),sin(65536.));
- for (i=0;i<128;i++)
-     {printf("sin(%8x)=%a sin(%8x)=%a\n",i<<24,sin(i<<24),(i+128)<<24,sin((i+128)<<24));
-      printf("cos(%8x)=%a cos(%8x)=%a\n",i<<24,cos(i<<24),(i+128)<<24,cos((i+128)<<24));
-      printf("abs(cis(%8x))=%a\n",i<<24,hypot(cos(i<<24),sin((i+128)<<24)));
-      }
+ testintegertrig();
  fclose(randfil);
  return EXIT_SUCCESS;
  }
