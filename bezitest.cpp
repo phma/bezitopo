fe5330e4bfb6ee4323aed13bd85883a1081b9061
Pierre Abbat <phma@phma.optus.nu>
2012-08-10 00:15:13 -0400

    test succeeds

--- bezitest.cpp
+++ bezitest.cpp
@@ -24,10 +24,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {int i,j,itype;
+ randfil=fopen("/dev/urandom","rb");
  xy a(0,0),b(4,0),c(0,3),d(4,4),e;
  //printf("Area is %7.3f\n",area3(c,a,b));
- //lozenge(100);
- //rotate(30);
+ assert(area3(c,a,b)==6);
+ lozenge(100);
+ rotate(30);
  printf("sin(int)=%f sin(float)=%f\n",sin(65536),sin(65536.));
  for (i=0;i<128;i++)
      {printf("sin(%8x)=%a sin(%8x)=%a\n",i<<24,sin(i<<24),(i+128)<<24,sin((i+128)<<24));
