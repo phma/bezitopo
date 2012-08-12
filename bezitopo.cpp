680442b405d00e3a91c6a20f0c59d58c89dd0c7c
Pierre Abbat <phma@phma.optus.nu>
2012-08-12 06:50:53 -0400

    production code works again

--- bezitopo.cpp
+++ bezitopo.cpp
@@ -19,23 +19,25 @@
 #include "measure.h"
 #include "pnezd.h"
 #include "angle.h"
+#include "pointlist.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
-{int i,j,itype;
- randfil=fopen("/dev/urandom","rb");
- set_length_unit(SURVEYFOOT);
- if (readpnezd("topo0.asc")<0)
-   readpnezd("../topo0.asc");
- rotate(2);
- /*for (i=0;i<9;i++)
-     for (j=0;j<=i;j++)
-         {itype=intersection_type(points[i],points[i+1],points[j],points[j+1]);
-          e=intersection(points[i],points[i+1],points[j],points[j+1]);
-          printf("i=%d j=%d Intersection type %d\nIntersection is (%f,%f)\n",i,j,itype,e.east(),e.north());
-          }*/
- maketin();
- fclose(randfil);
- return EXIT_SUCCESS;
- }
+{
+  int i,j,itype;
+  criteria crit;
+  criterion crit1;
+  randfil=fopen("/dev/urandom","rb");
+  set_length_unit(SURVEYFOOT);
+  if (readpnezd("topo0.asc")<0)
+    readpnezd("../topo0.asc");
+  crit1.str="";
+  crit1.istopo=true;
+  crit.push_back(crit1);
+  copytopopoints(crit);
+  rotate(2);
+  maketin();
+  fclose(randfil);
+  return EXIT_SUCCESS;
+}
