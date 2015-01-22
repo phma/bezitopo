e92388f81bcdf8906875c88befe842d11d011f2e
Pierre Abbat <phma@bezitopo.org>
2015-01-22 15:07:40 +0000

    another kind of surface

--- surface.h
+++ surface.h
@@ -5,8 +5,9 @@
 /******************************************************/
 /* A surface is a function from a region of 2-space to a scalar elevation.
  * Types of surface include:
- * * a hemisphere (for testing the area computation routine)
+ * * a hemisphere (for testing the volume computation routine)
  * * a TIN with its Bézier triangles
  * * an alignment with its cross-sections
+ * * the nth lowest of m surfaces
  * * a difference between surfaces.
  */
