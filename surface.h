a9e287d5dec2ac67c9eb0448f812593aa88189fe
Pierre Abbat <phma@bezitopo.org>
2015-01-22 10:36:10 +0000

    planning comment: add description of surfaces

---
+++ surface.h
@@ -0,0 +1,12 @@
+/******************************************************/
+/*                                                    */
+/* surface.h - surfaces                               */
+/*                                                    */
+/******************************************************/
+/* A surface is a function from a region of 2-space to a scalar elevation.
+ * Types of surface include:
+ * * a hemisphere (for testing the area computation routine)
+ * * a TIN with its Bézier triangles
+ * * an alignment with its cross-sections
+ * * a difference between surfaces.
+ */
