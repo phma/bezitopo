2c4042a3f3884bae9f4492247006a75dcf9f44fa
Pierre Abbat <phma@bezitopo.org>
2015-01-26 06:59:17 +0000

    vextrema returns exact 0 or 1 if slope at start or end is 0

--- vcurve.cpp
+++ vcurve.cpp
@@ -73,6 +73,13 @@ double vlength(double a,double b,double c,double d,double hlength)
 }
 
 vector<double> vextrema(double a,double b,double c,double d)
+/* This is used for two quite different purposes:
+ * 1. to place storm sewer grates at the lowest point along a street
+ * 2. to divide sides of a triangle into monotonic parts and decide which of
+ *    two crossing segments to delete when subdividing a triangle.
+ * For 1, extrema at the endpoints count. For 2, they do not. Extrema
+ * at the endpoints must therefore be placed exactly at the endpoints.
+ */
 {
   double slopes[3],quad,vertex,minslope,root,discriminant;
   vector<double> ext;
@@ -87,16 +94,40 @@ vector<double> vextrema(double a,double b,double c,double d)
     minslope=fabs(slopes[2]);
   if (quad) //TODO: improve numerical stability in the case that one root is in [0,1] and the other is far away
   {
-    if (minslope<=fabs(quad)/4 || slopes[0]*slopes[2]<=0)
+    if (minslope==0 && slopes[1]!=0)
+    {
+      if (slopes[0]==0 && slopes[2]==0)
+      {
+	ext.push_back(0);
+	ext.push_back(1);
+      }
+      else
+      {
+	vertex=0.5-(slopes[2]-slopes[0])*0.25/quad;
+	if (slopes[0]==0)
+	{
+	  ext.push_back(0);
+	  if (vertex<0.5 && vertex>0)
+	    ext.push_back(2*vertex);
+	}
+	if (slopes[2]==0)
+	{
+	  if (vertex>0.5 && vertex<1)
+	    ext.push_back(2*vertex-1);
+	  ext.push_back(1);
+	}
+      }
+    }
+    else if (minslope<=fabs(quad)/4 || slopes[0]*slopes[2]<=0)
     {
       vertex=0.5-(slopes[2]-slopes[0])*0.25/quad;
       discriminant=-vslope(a,b,c,d,vertex)/quad/2;
       if (discriminant>=0)
       {
-        root=vertex+sqrt(discriminant);
+        root=vertex-sqrt(discriminant);
 	if (root>=0 && root<=1)
 	  ext.push_back(root);
-	root=vertex-sqrt(discriminant);
+	root=vertex+sqrt(discriminant);
 	if (root>=0 && root<=1 && discriminant)
 	  ext.push_back(root);
       }
