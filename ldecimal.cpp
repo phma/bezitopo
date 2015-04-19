cfb6966a71bbe3978093df5346932d6c49d018aa
Pierre Abbat <phma@bezitopo.org>
2015-04-19 18:06:10 +0000

    idecimal removes the exponent if that would make it no longer

--- ldecimal.cpp
+++ ldecimal.cpp
@@ -16,7 +16,7 @@ string ldecimal(double x)
   int i,iexp;
   size_t zpos;
   char *dotpos,*epos;
-  string ret,m,antissa,exponent;
+  string ret,s,m,antissa,exponent;
   char buffer[32],fmt[8];
   for (i=DBL_DIG-1,x2=-1/x;x!=x2 && i<DBL_DIG+3;i++)
   {
@@ -31,10 +31,44 @@ string ldecimal(double x)
     m=string(buffer,dotpos-buffer);
     antissa=string(dotpos+1,epos-dotpos-1);
     exponent=string(epos+1);
+    if (m.length()>1)
+    {
+      s=m.substr(0,1);
+      m.erase(0,1);
+    }
     iexp=atoi(exponent.c_str());
     zpos=antissa.find_last_not_of('0');
     antissa.erase(zpos+1);
-    ret=m+'.'+antissa+'e'+exponent;
+    iexp=stoi(exponent);
+    if (iexp<0 && iexp>-5)
+    {
+      antissa=m+antissa;
+      m="";
+      iexp++;
+    }
+    if (iexp>0 && iexp<=antissa.length())
+    {
+      m+=antissa.substr(0,iexp);
+      antissa.erase(0,iexp);
+      iexp=0;
+    }
+    while (iexp>-5 && iexp<0 && m.length()==0)
+    {
+      antissa="0"+antissa;
+      iexp++;
+    }
+    while (iexp<3 && iexp>0 && antissa.length()==0)
+    {
+      m+='0';
+      iexp--;
+    }
+    sprintf(buffer,"%d",iexp);
+    exponent=buffer;
+    ret=s+m;
+    if (antissa.length())
+      ret+='.'+antissa;
+    if (iexp)
+      ret+='e'+exponent;
   }
   else
     ret=buffer;
