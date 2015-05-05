5405e2e097061846c7c86512e275adb85d0c1420
Pierre Abbat <phma@bezitopo.org>
2015-05-05 08:55:12 +0000

    comments on projections

---
+++ projection.h
@@ -0,0 +1,23 @@
+/******************************************************/
+/*                                                    */
+/* projection.h - map projections                     */
+/*                                                    */
+/******************************************************/
+/* All map projections used in surveying are conformal. These are in use:
+ * • Lambert conformal conic
+ * • Stereographic at the poles
+ * • Axial Mercator, possibly
+ * • Transverse Mercator
+ * • Oblique Mercator, in the Alaska panhandle
+ * 
+ * Lambert conformal conic on the ellipsoid can be done by conformally projecting
+ * the ellipsoid to a sphere and using the spherical formula for the projection.
+ * This does not work for transverse Mercator: the result is conformal, but
+ * the scale on the meridian is not constant. As to oblique Mercator, I'll let
+ * the Alaskan surveyors figure that one out.
+ * 
+ * Stereographic and axial Mercator are special cases of conformal conic.
+ * Stereographic does not require checking, but Mercator does, otherwise
+ * it results in raising a complex number to the 0 power, subtracting 1, and
+ * dividing by 0.
+ */
