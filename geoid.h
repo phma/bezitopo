6873084922018a5347cf6dcdc307811b0372cc97
Pierre Abbat <phma@bezitopo.org>
2015-09-18 04:03:11 +0000

    add stub geoid.h

---
+++ geoid.h
@@ -0,0 +1,8 @@
+/******************************************************/
+/*                                                    */
+/* geoid.h - geoidal undulation                       */
+/*                                                    */
+/******************************************************/
+/* http://www.hypack.com/new/portals/1/PDF/sb/07_10/New Geoid Model for France and Information about Geoid File Formats.pdf
+ * describes some geoid file formats.
+ */
