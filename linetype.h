79018454cc880747d6b3e4868c74ad9b7c3fde0d
Pierre Abbat <phma@bezitopo.org>
2016-04-28 23:54:16 +0000

    copyright notices in more files

--- linetype.h
+++ linetype.h
@@ -3,6 +3,22 @@
 /* linetype.h - dash patterns and line widths         */
 /*                                                    */
 /******************************************************/
+/* Copyright 2015 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* Dash patterns are encoded as a string of bytes, where:
  * 0xff 1024 mm
--- measure.cpp
+++ measure.cpp
@@ -3,6 +3,22 @@
 /* measure.cpp - measuring units                      */
 /*                                                    */
 /******************************************************/
+/* Copyright 2012,2015 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
 /* The unit of length can be the foot, chain, or meter. Independently
  * of this, the foot can be selected from the international foot,
  * the US survey foot, or maybe the Indian survey foot. Selecting
