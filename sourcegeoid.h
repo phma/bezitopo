cd1c8c6b7f781a3812cd85b2273f2b3d64224889
Pierre Abbat <phma@bezitopo.org>
2016-05-11 21:00:00 +0000

    finish adding copyright notices to files

--- sourcegeoid.h
+++ sourcegeoid.h
@@ -3,6 +3,22 @@
 /* sourcegeoid.h - geoidal undulation source data     */
 /*                                                    */
 /******************************************************/
+/* Copyright 2015,2016 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
 /* http://www.hypack.com/new/portals/1/PDF/sb/07_10/New Geoid Model for France and Information about Geoid File Formats.pdf
  * describes some geoid file formats. These are latitude-longitude grids.
  * I don't know what formats are used for polar geoid files.
--- test.h
+++ test.h
@@ -3,6 +3,22 @@
 /* test.h - test patterns and functions               */
 /*                                                    */
 /******************************************************/
+/* Copyright 2012,2013,2014,2015 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "document.h"
 
