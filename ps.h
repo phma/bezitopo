634e7ac30a6ed6befca3ffadd26e725fa51451ea
Pierre Abbat <phma@bezitopo.org>
2016-12-18 11:13:15 +0000

    Delete all non-class PostScript functions.

--- ps.h
+++ ps.h
@@ -61,20 +61,4 @@ public:
   void comment(string text);
 };
 
-/*extern FILE *psfile;
-extern int orientation;
-void psprolog();
-void startpage();
-void endpage();
-void dot(xy pnt,std::string comment="");
-void circle(xy pnt,double radius);
-void line(document &doc,edge lin,int num,bool colorfibaster,bool directed=false);
-void pstrailer();
-void line2p(xy pnt1,xy pnt2);
-void spline(bezier3d spl);
-void widen(double factor);
-void setcolor(double r,double g,double b);
-void setscale(double minx,double miny,double maxx,double maxy,int ori=0);
-void pswrite(xy pnt,std::string text);
-void pscomment(string text);*/
 #endif
