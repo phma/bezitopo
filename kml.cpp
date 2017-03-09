36d31bc7f7149f63c2554788ea1510c2f865b0a7
Pierre Abbat <phma@bezitopo.org>
2017-03-09 04:11:45 +0000

    start kml.cpp

---
+++ kml.cpp
@@ -0,0 +1,42 @@
+/******************************************************/
+/*                                                    */
+/* kml.cpp - Keyhole Markup Language                  */
+/*                                                    */
+/******************************************************/
+/* Copyright 2017 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* Writes the boundary of an excerpt of a geoid file to a KML file
+ * so that it can be seen on a map.
+ */
+
+#include "kml.h"
+using namespace std;
+
+void openkml(ofstream &file,char *filename)
+{
+  file.open(filename);
+  file<<"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+      <<"<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"
+      <<"<Document>\n";
+}
+
+void closekml(ofstream &file)
+{
+  file<<"</Document></kml>\n";
+  file.close();
+}
+
