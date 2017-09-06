207e97dcf92de0931542a7fb87a3665b4440a71b
Pierre Abbat <phma@bezitopo.org>
2017-09-06 02:57:16 +0000

    stub GUI programs

---
+++ viewtin.cpp
@@ -0,0 +1,32 @@
+/******************************************************/
+/*                                                    */
+/* viewtin.cpp - view triangulated irregular network  */
+/*                                                    */
+/******************************************************/
+/* Copyright 2017 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <QtGui>
+#include <QtWidgets>
+#include "tinwindow.h"
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+  TinWindow window;
+  window.show();
+  return app.exec();
+}
