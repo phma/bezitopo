42ce7f3d41445354ac2d36c30a533d19b9b312f6
Pierre Abbat <phma@bezitopo.org>
2017-10-10 17:48:35 +0000

    planning comment for transverse Mercator

--- projection.h
+++ projection.h
@@ -112,3 +112,13 @@ protected:
 };
 
 extern StereographicSphere sphereStereoArabianSea;
+
+/* This is how I'll do transverse Mercator:
+ * Transverse Mercator on a sphere is easy. Transverse Mercator on an ellipsoid
+ * can be computed as the convolution of two conformal maps: transverse Mercator
+ * on a sphere and a map that corrects the length along the central meridian.
+ * The length along the central meridian of an ellipsoid is an elliptic integral,
+ * which can be approximated with many sufficiently small pieces. Then compute
+ * the function to correct the length as a Fourier series. It is automatically
+ * analytic, hence conformal, and the scale can be computed by differentiating it.
+ */
