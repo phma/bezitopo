2f32a2fd7619833a82d7a08adef6f8a18c93eaf0
Pierre Abbat <phma@bezitopo.org>
2017-12-04 05:12:06 +0000

    RenderCache (not finished)

---
+++ rendercache.cpp
@@ -0,0 +1,79 @@
+/******************************************************/
+/*                                                    */
+/* rendercache.cpp - cache of renderings of drawobjs  */
+/*                                                    */
+/******************************************************/
+/* Copyright 2017 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "rendercache.h"
+using namespace std;
+
+void RenderCache::clear()
+{
+  renderMap.clear();
+}
+
+void RenderCache::clearPresent()
+{
+  map<drawobj *,RenderItem>::iterator i;
+  int j,nsteps;
+  for (i=renderMap.begin();i!=renderMap.end();i++)
+    i->second.present=false;
+  nsteps=rng.ucrandom();
+  for (j=0;j<nsteps;j++)
+    subrand.scalar(1);
+}
+
+void RenderCache::deleteAbsent()
+{
+  map<drawobj *,RenderItem>::iterator i;
+  int j;
+  vector<drawobj *> delenda;
+  for (i=renderMap.begin();i!=renderMap.end();i++)
+    if (!i->second.present)
+      delenda.push_back(i->first);
+  for (j=0;j<delenda.size();j++)
+    renderMap.erase(delenda[j]);
+}
+
+bool RenderCache::shouldRerender(double oldScale,double newScale)
+{
+  double ratio=oldScale/newScale;
+  double prob=ratio*exp(1-ratio);
+  return prob<1 && subrand.scalar(1)>=prob;
+}
+
+void RenderCache::checkInObject(drawobj *obj,double pixelScale,unsigned short ltype,unsigned short colr,unsigned short thik)
+{
+  unsigned objHash;
+  if (!renderMap.count(obj))
+    renderMap[obj].pixelScale=INFINITY;
+  renderMap[obj].ltype=ltype;
+  renderMap[obj].colr=colr;
+  renderMap[obj].thik=thik;
+  renderMap[obj].present=true;
+  objHash=obj->hash(); // Computing the hash of a large polyspiral takes 1/20 as much time as rendering it.
+  if (shouldRerender(renderMap[obj].pixelScale,pixelScale) ||
+      objHash!=renderMap[obj].hash)
+  {
+    renderMap[obj].rendering=obj->render3d(pixelScale);
+    renderMap[obj].pixelScale=pixelScale;
+    renderMap[obj].hash=objHash;
+  }
+}
+
+      
---
+++ rendercache.h
@@ -0,0 +1,62 @@
+/******************************************************/
+/*                                                    */
+/* rendercache.h - cache of renderings of drawobjs    */
+/*                                                    */
+/******************************************************/
+/* Copyright 2017 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <map>
+#include "drawobj.h"
+#include "halton.h"
+#include "random.h"
+
+/* The RenderCache is used for all objects visible in a canvas EXCEPT:
+ * • points, which are drawn as three concentric circles sized in pixels;
+ * • edges, which are not drawing objects and which are drawn as just one (sp)line.
+ * If an object hasn't changed (as shown by its hash), and the pixel scale
+ * is the same as when it was last rendered, the rendering is left alone.
+ * If the scale has changed, it is rerendered with a probability depending
+ * on the ratio. If the object has changed, it is always rerendered.
+ * 
+ * This works well, except in some cases involving blocks. If a block in the
+ * drawing layer has a line in the setback layer, and you hide the setback
+ * layer, the line will remain visible until you rerender everything or the
+ * block is rerendered because the scale changes.
+ */
+
+class RenderItem
+{
+public:
+  unsigned short ltype,colr,thik;
+  bool present;
+  unsigned hash;
+  double pixelScale;
+  std::vector<drawingElement> rendering;
+};
+
+class RenderCache
+{
+private:
+  halton subrand;
+  std::map<drawobj *,RenderItem> renderMap;
+  bool shouldRerender(double oldScale,double newScale);
+public:
+  void clear();
+  void clearPresent();
+  void deleteAbsent();
+  void checkInObject(drawobj *obj,double pixelScale,unsigned short ltype,unsigned short colr,unsigned short thik);
+};
