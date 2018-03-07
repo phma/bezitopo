fd5b1dc2694273a9a1e95636e808b3eced42e1d1
Pierre Abbat <phma@bezitopo.org>
2018-03-07 22:44:05 +0000

    Stub BoundRect class

---
+++ boundrect.cpp
@@ -0,0 +1,56 @@
+/******************************************************/
+/*                                                    */
+/* boundrect.cpp - bounding rectangles                */
+/*                                                    */
+/******************************************************/
+/* Copyright 2018 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "boundrect.h"
+
+BoundRect::BoundRect()
+{
+  int i;
+  for (i=0;i<4;i++)
+    bounds[i]=INFINITY;
+  orientation=0;
+}
+
+BoundRect::BoundRect(int ori)
+{
+  int i;
+  for (i=0;i<4;i++)
+    bounds[i]=INFINITY;
+  orientation=ori;
+}
+
+void BoundRect::clear()
+{
+  int i;
+  for (i=0;i<4;i++)
+    bounds[i]=INFINITY;
+}
+
+void BoundRect::setOrientation(int ori)
+{
+  orientation=ori;
+}
+
+int BoundRect::getOrientation()
+{
+  return orientation;
+}
+
---
+++ boundrect.h
@@ -0,0 +1,35 @@
+/******************************************************/
+/*                                                    */
+/* boundrect.h - bounding rectangles                  */
+/*                                                    */
+/******************************************************/
+/* Copyright 2018 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "pointlist.h"
+
+class BoundRect
+{
+private:
+  int orientation;
+  std::array<double,4> bounds;
+public:
+  BoundRect();
+  BoundRect(int ori);
+  void clear();
+  void setOrientation(int ori);
+  int getOrientation();
+};
