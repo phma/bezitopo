6a47cf8930153e6ef86af66a7af4eb9d1fee05f5
Pierre Abbat <phma@bezitopo.org>
2018-06-25 03:09:43 +0000

    Draw the envelope of the lines connecting the two circles

--- bezitest.cpp
+++ bezitest.cpp
@@ -1682,7 +1682,9 @@ void testcircle()
   Circle yaxis(xy(0,0),DEG90,0);
   PostScript ps;
   int i,shortCount=0,longCount=0;
-  xy sta1,sta3;
+  xyz sta1,sta3;
+  vector<segment> lines;
+  polyline envelope;
   ps.open("circle.ps");
   ps.setpaper(papersizes["A4 portrait"],0);
   ps.prolog();
@@ -1697,13 +1699,19 @@ void testcircle()
       shortCount++;
     if (dist(sta1,sta3)>3.162278) // The distance at 45°/135° is sqrt(10).
       longCount++;
+    lines.push_back(segment(sta1,sta3));
   }
   tassert(shortCount==longCount);
   ps.spline(unit.approx3d(0.1/ps.getscale()));
   ps.spline(triple.approx3d(0.1/ps.getscale()));
+  ps.setcolor(1,0,1);
+  for (i=0;i<lines.size();i++)
+    envelope.insert(intersection(lines[i],lines[(i+1)%lines.size()]));
+  ps.spline(envelope.approx3d(0.1/ps.getscale()));
   ps.endpage();
   ps.startpage();
   ps.setscale(-1,-1,1,1,degtobin(0));
+  ps.setcolor(0,0,0);
   for (i=8388608-DEG180;i<=DEG180;i+=16777216)
     ps.line2p(xaxis.station(tan(i)),yaxis.station(cot(i)));
   ps.spline(xaxis.approx3d(0.1/ps.getscale()));
