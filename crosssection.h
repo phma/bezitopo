532af98b65245522e02f8638b9db555a00d7923f
Pierre Abbat <phma@bezitopo.org>
2019-07-24 07:09:39 +0000

    Add stub of abstract BaseCrossSection class

--- crosssection.h
+++ crosssection.h
@@ -3,7 +3,7 @@
 /* crosssection.h - cross sections of roadways        */
 /*                                                    */
 /******************************************************/
-/* Copyright 2013 Pierre Abbat.
+/* Copyright 2013,2019 Pierre Abbat.
  * This file is part of Bezitopo.
  * 
  * Bezitopo is free software: you can redistribute it and/or modify
@@ -20,7 +20,10 @@
  * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* A cross section is taken at a station and consists of a list
+/* There are two kinds of cross section: TIN cross sections, which
+ * are taken of a TIN and design cross sections, which specify the
+ * design of a road.
+ * A design cross section is taken at a station and consists of a list
  * of triples (index,offset,elevation), where index is a dimensionless
  * number and offset and elevation are in meters. By convention,
  * index=0 where offset=0, which is the centerline. index is used to
@@ -28,13 +31,28 @@
  * elevation is relative to the alignment.
  */
 #include <map>
+#include "xyz.h"
 
 struct xsitem
 {
   double offset,elevation;
-}
+};
 
-class crosssection
+class BaseCrossSection
 {
-  map<double,xsitem> data;
-}
+public:
+  void setVerticalOffset(double o);
+  double getVerticalOffset();
+  virtual double elevation(double o)=0;
+  virtual double lowElevation(double o)=0;
+  virtual double highElevation(double o)=0;
+protected:
+  xy midpoint;
+  double verticalOffset;
+  int direction;
+};
+
+class DesignCrossSection: public BaseCrossSection
+{
+  std::map<double,xsitem> data;
+};
