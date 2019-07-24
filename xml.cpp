0d5ce3c337b7a749d38898167b96b3a2becc9a5d
Pierre Abbat <phma@bezitopo.org>
2019-07-24 07:27:22 +0000

    Add xml.h, xml.cpp (no code yet)

---
+++ xml.cpp
@@ -0,0 +1,25 @@
+/******************************************************/
+/*                                                    */
+/* xml.cpp - parse XML                                */
+/*                                                    */
+/******************************************************/
+/* Copyright 2019 Pierre Abbat.
+ * This file is part of Bezitopo.
+ * 
+ * Bezitopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bezitopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* This is a quick&dirty module I'm hacking up to parse the XML in KML files.
+ * The files are less than 200 kB, small enough to read into RAM and
+ * parse there.
+ */
