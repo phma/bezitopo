9f60b9d0b019fdda199fd16eae6f4c8df6f432a8
Pierre Abbat <phma@bezitopo.org>
2019-09-22 12:00:54 -0400

    Define a spiralarc that GCC can handle but MSVC can't

--- bezitest.cpp
+++ bezitest.cpp
@@ -91,7 +91,8 @@
 
 #define CBRT2 1.2599210498948731647592197537
 
-#define tassert(x) testfail|=(!(x))
+//#define tassert(x) testfail|=(!(x))
+#define tassert(x) assert(x)
 //#define tassert(x) if (!(x)) {testfail=true; sleep(10);}
 // so that tests still work in non-debug builds
 
@@ -2683,7 +2684,7 @@ void testcurly()
 {
   int i;
   double curvature,clothance;
-  spiralarc s;
+  spiralarc s(xyz(0,0,0),0,28.25,0,-1.5,1.5); // works in GCC, fails in MSVC
   PostScript ps;
   ps.open("curly.ps");
   ps.setpaper(papersizes["A4 landscape"],0);
