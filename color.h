360241932419817badac8917f7c5f31f1501bb80
Pierre Abbat <phma@bezitopo.org>
2019-11-28 11:29:32 +0000

    Change license to LGPL

--- color.h
+++ color.h
@@ -5,19 +5,21 @@
 /******************************************************/
 /* Copyright 2015,2017 Pierre Abbat.
  * This file is part of Bezitopo.
- * 
+ *
  * Bezitopo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * Bezitopo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ * and Lesser General Public License along with Bezitopo. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 /* Colors are stored in two formats: short and int.
  * A short denotes one of 64000 colors (40³) or any of up to 1536 special values
--- measure.h
+++ measure.h
@@ -5,19 +5,21 @@
 /******************************************************/
 /* Copyright 2012,2015-2019 Pierre Abbat.
  * This file is part of Bezitopo.
- * 
+ *
  * Bezitopo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * Bezitopo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Bezitopo. If not, see <http://www.gnu.org/licenses/>.
+ * and Lesser General Public License along with Bezitopo. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #ifndef MEASURE_H
