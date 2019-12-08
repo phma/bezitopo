76e200acda31b52071886c0d731cb0ff99239023
Pierre Abbat <phma@bezitopo.org>
2019-12-08 10:15:35 +0000

    Check for NaN in triangulatePolygon
    This fixes a hang found by fuzzing.

--- tin.cpp
+++ tin.cpp
@@ -1201,7 +1201,7 @@ void pointlist::triangulatePolygon(vector<point *> poly)
   ymean=pairwisesum(coords);
   coords.clear();
   startpnt=xy(xmean,ymean);
-  while (sz>2)
+  while (sz>2 && startpnt.isfinite())
   {
     for (isInside=i=0;i<sz;i++)
       isInside+=foldangle(dir(startpnt,*poly[(i+1)%sz])-dir(startpnt,*poly[i]));
